792df907c7e2bef061e9cfb8649448e5714fab18
kaita49 <135412298+kaita49@users.noreply.github.com>
2024-01-15 16:56:53 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4fHQHd4QfJJ7O8pFAIFQ-llNB6MemAOE",
-  authDomain: "lender-d2825.firebaseapp.com",
-  projectId: "lender-d2825",
-  storageBucket: "lender-d2825.appspot.com",
-  messagingSenderId: "401852318724",
-  appId: "1:401852318724:web:70e62d25e8315e1217a151",
-  measurementId: "G-04KVJ6G636"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
